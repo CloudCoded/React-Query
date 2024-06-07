1434da022c817089ecf8dbef5299ea65477475c2
zeeze <abdulazeezakindolapo@gmail.com>
2024-06-07 17:53:01 +0100

    error handling in react-query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -3,15 +3,17 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 
 const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes')
+  return axios.get('http://localhost:4000/superheroes1')
 }
 
 export const RQSuperHeroes = () => {
-  const {isLoading, data} =useQuery('super-heroes', fetchSuperHeroes)
+  const {isLoading, data, isError, error} =useQuery('super-heroes', fetchSuperHeroes)
 
   if (isLoading) {
     <h2>Loading</h2>
   }
+
+  {isError && <h2>{error.message}</h2>}
 
   return (
     <>
