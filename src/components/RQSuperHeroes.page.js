eff5dde4eff98b287a3b9a68ba05982df5d23800
zeeze <abdulazeezakindolapo@gmail.com>
2024-06-27 11:57:44 +0100

    callback

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -5,9 +5,16 @@ import axios from 'axios'
 const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes')
 }
-
+ 
 export const RQSuperHeroes = () => {
-  const {isLoading, data, isError, error, isFetching, refetch} =useQuery('super-heroes', fetchSuperHeroes, { enabled: false})
+  const onSuccess = () => {
+    console.log("data fetching successfully")
+  }
+
+  const onError = () => {
+    console.log("data fetching failed")
+  }
+  const {isLoading, data, isError, error, isFetching, refetch} =useQuery('super-heroes', fetchSuperHeroes, {onSuccess, onError}, { enabled: false})
   console.log(isLoading, isFetching)
 
   if (isLoading) {
